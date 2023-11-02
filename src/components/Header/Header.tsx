1892fdf5e98ffdd52a64349d95146502e6d4ca88
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2023-11-02 20:51:59 +0100

    feature: add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { HeaderProps } from "./Header.types";
+import styles from "./Header.module.scss";
+
+export const Header: FC<HeaderProps> = ({ labels }) => {
+  return (
+    <div
+      className={styles.container}
+      style={{
+        animationDelay: `${0.3 * labels.length}s`,
+      }}
+    >
+      <div className={styles.labels}>
+        {labels.map((label) => (
+          <p key={label}>{label}</p>
+        ))}
+      </div>
+      <div className={styles.score}>
+        <p className={styles.label}>Score</p>
+        <p className={styles.value}>0</p>
+      </div>
+    </div>
+  );
+};
