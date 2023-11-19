47682c17e43ce6c2ef30d3d69062b6a2356d2600
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2023-11-19 14:06:18 +0100

    feature: implemented particles in arena + fancy animations

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,35 +1,16 @@
 import { FC, useState } from "react";
-import { Selection, Header } from "@/components";
+import { Selection, Header, Arena } from "@/components";
 import { moves } from "@/data/moves";
-import { Arena } from "@/components";
 import { Move } from "@/types";
 import styles from "./App.module.scss";
+import { Reset } from "./Reset";
 
 export const App: FC = () => {
   const [selected, setSelected] = useState<Move | null>(null);
 
   return (
     <div className={styles.container}>
-      <div
-        style={{
-          position: "absolute",
-          top: "10px",
-          right: "10px",
-          width: "100px",
-          height: "50px",
-          cursor: "pointer",
-          border: "1px solid black",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          borderRadius: "5px",
-          background: "rgba(0,0,0,0.5)",
-          lineHeight: "1",
-        }}
-        onClick={() => setSelected(null)}
-      >
-        RESET
-      </div>
+      <Reset onClick={() => setSelected(null)} />
       <Header labels={moves.map((m) => m.name)} />
       <div className={styles.selection}>
         <Selection selected={selected} moves={moves} onSelect={setSelected} />
--- src/components/Arena/Arena.tsx
+++ src/components/Arena/Arena.tsx
@@ -1,15 +1,22 @@
 import { FC, useEffect, useMemo, useState } from "react";
+import classNames from "classnames";
+import { Particles } from "@/components";
 import { moves } from "@/data/moves";
 import styles from "./Arena.module.scss";
 import { Move } from "@/types";
 
 export const Arena: FC<{ selected: Move }> = ({ selected }) => {
-  const [index, setIndex] = useState<number>(0);
-
+  const [index, setIndex] = useState(0);
+  const [reveal, setReveal] = useState(false);
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((p) => (p + 1 === moves.length ? 0 : p + 1));
     }, 100);
+    setTimeout(() => {
+      clearInterval(interval);
+      setIndex(Math.floor(Math.random() * moves.length));
+      setReveal(true);
+    }, 5000);
     return () => clearInterval(interval);
   }, []);
 
@@ -19,11 +26,33 @@ export const Arena: FC<{ selected: Move }> = ({ selected }) => {
   }, [index]);
 
   return (
-    <div className={styles.arena}>
-      <div className={styles.selected}>
-        <selected.Icon />
+    <>
+      {reveal
+        ? selected.name === moves[index].name
+          ? "You draw!"
+          : selected.beats.includes(moves[index].name)
+          ? "You win!"
+          : "You lose!"
+        : "Waiting for opponent..."}
+      <div className={styles.arena}>
+        <div className={styles.selected}>
+          <selected.Icon />
+        </div>
+        <div
+          className={classNames(
+            styles.opponentContainer,
+            reveal && styles.reveal
+          )}
+        >
+          <div className={styles.particles}>
+            <Particles
+              color={reveal ? "150, 0, 0" : "255, 255, 255"}
+              speed={reveal ? 20 : 3}
+            />
+          </div>
+          <div className={styles.opponent}>{Random}</div>
+        </div>
       </div>
-      <div className={styles.opponent}>{Random}</div>
-    </div>
+    </>
   );
 };
